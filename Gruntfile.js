9915133b8cf3cfaaa86080ccd1c5ab12609b891e
Kenneth Baylosis <kbaylosis@zoogtech.com>
2016-01-31 04:26:43 +0800

    Fix grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,9 @@ module.exports = function(grunt) {
 		pkg: grunt.file.readJSON('package.json'),
 		uglify: {
 			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+				compress : true,
+				mangle : true
 			},
 			build: {
 				src: 'index.js',
